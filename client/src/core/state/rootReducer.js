92c72ddaeedd76b742c6bd506a9831247e429abc
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2018-11-14 15:57:26 +0100

    Removes react-router-redux packpage

--- client/src/core/state/rootReducer.js
+++ client/src/core/state/rootReducer.js
@@ -1,11 +1,9 @@
 import { combineReducers } from "redux";
-import { routerReducer } from "react-router-redux";
 
 import postsReducer from "../../features/posts/reducers/postsReducer";
 
 const rootReducer = combineReducers({
-  posts: postsReducer,
-  router: routerReducer
+  posts: postsReducer
 });
 
 export default rootReducer;

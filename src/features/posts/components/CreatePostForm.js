35707334dfb4f1c7060b79868068483be32337f0
Amer Karamustafic <26983729+malcodeman@users.noreply.github.com>
2020-04-18 23:29:33 +0200

    Upgrades formik in create post form

--- src/features/posts/components/CreatePostForm.js
+++ src/features/posts/components/CreatePostForm.js
@@ -1,33 +1,33 @@
 import React from "react";
-import { withFormik, Form, Field } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 import styled from "styled-components";
-import { connect } from "react-redux";
+import { useDispatch } from "react-redux";
 
 import { createPost } from "../actions/postsActions";
 
-const StyledForm = styled(Form)`
+const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
 `;
 
-const Input = styled(Field)`
+const Input = styled.input`
   height: 36px;
   font-size: 0.8rem;
   padding: 0 4px;
   outline: 0;
   border: 0;
   margin-bottom: 10px;
-  color: ${props => props.theme.primary};
-  border-radius: ${props => props.theme.borderRadius};
-  border: 1px solid ${props => props.theme.backgroundPrimary};
-  background-color: ${props => props.theme.backgroundPrimary};
+  color: ${(props) => props.theme.primary};
+  border-radius: ${(props) => props.theme.borderRadius};
+  border: 1px solid ${(props) => props.theme.backgroundPrimary};
+  background-color: ${(props) => props.theme.backgroundPrimary};
   :focus {
-    border: 1px solid ${props => props.theme.brand};
-    background-color: ${props => props.theme.backgroundSecondary};
+    border: 1px solid ${(props) => props.theme.brand};
+    background-color: ${(props) => props.theme.backgroundSecondary};
   }
   ::placeholder {
-    color: ${props => props.theme.placeholder};
+    color: ${(props) => props.theme.placeholder};
   }
 `;
 
@@ -37,8 +37,8 @@ const Error = styled.div`
   margin: 4px 0;
   align-self: flex-start;
   color: #fff;
-  background-color: ${props => props.theme.error};
-  border-radius: ${props => props.theme.borderRadius};
+  background-color: ${(props) => props.theme.error};
+  border-radius: ${(props) => props.theme.borderRadius};
 `;
 
 const Submit = styled.button`
@@ -48,24 +48,56 @@ const Submit = styled.button`
   border: 0;
   align-self: flex-end;
   color: #fff;
-  background-color: ${props => props.theme.brand};
-  border-radius: ${props => props.theme.borderRadius};
+  background-color: ${(props) => props.theme.brand};
+  border-radius: ${(props) => props.theme.borderRadius};
   &:disabled {
     opacity: 0.6;
     cursor: default;
   }
 `;
 
-function FormikForm(props) {
-  const { errors, values, touched, isSubmitting } = props;
+function CreatePostForm(props) {
+  const dispatch = useDispatch();
+  const validationSchema = Yup.object().shape({
+    body: Yup.string()
+      .required("Can't be empty")
+      .max(280, "Max 280 characters"),
+  });
+  const formik = useFormik({
+    validationSchema,
+    initialValues: {
+      body: props.body || "",
+    },
+    onSubmit() {
+      dispatch(
+        createPost(formik.values, {
+          setSubmitting: formik.setSubmitting,
+          resetForm: formik.resetForm,
+        })
+      );
+    },
+  });
 
   return (
-    <StyledForm>
-      <Input type="text" name="body" placeholder="Your confession here" />
-      {touched.body && errors.body && <Error>{errors.body}</Error>}
+    <StyledForm onSubmit={formik.handleSubmit}>
+      <Input
+        type="text"
+        name="body"
+        placeholder="Your confession here"
+        value={formik.values.body}
+        onChange={formik.handleChange}
+        onBlur={formik.handleBlur}
+      />
+      {formik.touched.body && formik.errors.body && (
+        <Error>{formik.errors.body}</Error>
+      )}
       <Submit
         type="submit"
-        disabled={isSubmitting || errors.body || values.body.length === 0}
+        disabled={
+          formik.isSubmitting ||
+          formik.errors.body ||
+          formik.values.body.length === 0
+        }
       >
         Post
       </Submit>
@@ -73,24 +105,4 @@ function FormikForm(props) {
   );
 }
 
-const CreatePostForm = withFormik({
-  validationSchema: Yup.object().shape({
-    body: Yup.string()
-      .required("Can't be empty")
-      .max(280, "Max 280 characters")
-  }),
-  mapPropsToValues: props => ({
-    body: props.body || ""
-  }),
-  handleSubmit(payload, bag) {
-    bag.props.createPost(payload, {
-      setSubmitting: bag.setSubmitting,
-      resetForm: bag.resetForm
-    });
-  }
-})(FormikForm);
-
-export default connect(
-  null,
-  { createPost }
-)(CreatePostForm);
+export default CreatePostForm;
